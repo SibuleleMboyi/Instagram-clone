eeb5ba8e104f95bdbdb709816b231450cf8b036e
sibulele <smboyi2016@gmail.com>
2021-01-26 04:05:11 +0200

    Start of my Instagram Clone project. I have just finished both Login in and Sign Up functionalities.

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/screens/signup_screen.dart';
+import 'package:instagram/services/auth_service.dart';
+
+class LoginScreen extends StatefulWidget {
+  static final String id = 'login_screen';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState  extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _email, _password;
+
+  _submit(){
+    if(_formKey.currentState.validate()){
+      _formKey.currentState.save();
+      //print(_email);
+      //print(_password);
+      //Logging in the user with Firebase
+      AuthService.login(_email, _password);
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Instagram',
+                style: TextStyle(
+                  fontFamily: 'Billabong',
+                  fontSize: 50.0,
+
+                ),
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding:  EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Email'),
+                        validator: (input) => !input.contains('@') ? 'Please enter a valid email' : null,
+                        onSaved: (input) => _email = input,
+                      ),
+                    ),
+
+                    Padding(
+                      padding:  EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Password'),
+                        validator: (input) => input.length < 6 ? 'Must be at least 6 charecters' : null,
+                        onSaved: (input) => _password = input,
+                        obscureText: true,
+                      ),
+                    ),
+
+                    SizedBox(height: 20.0),
+                    Container(
+                      width: 250.0,
+                      child: FlatButton(
+                        onPressed: _submit,
+                        color: Colors.blue,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Container(
+                      width: 250.0,
+                      child: FlatButton(
+                        onPressed: () => Navigator.pushNamed(context, SignupScreen.id),
+                        color: Colors.blue,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          'Go to Signup',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
