99d4723cbf7a9e48d4eb530b5a1741d5995bdca9
sibulele <smboyi2016@gmail.com>
2021-01-27 12:25:30 +0200

    Finished working on the AppBar and BottomNavigationBar

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:instagram/screens/feed_screen.dart';
-import 'package:instagram/screens/login_screen.dart';
 
 class AuthService{
 
@@ -26,11 +25,15 @@ class AuthService{
     }
   }
 
-  static void logout(BuildContext context) {
+  static void logout() {
      _auth.signOut();
-       Navigator.pushReplacementNamed(context, LoginScreen.id);
   }
-  static void login(String email, String password){
-    _auth.signInWithEmailAndPassword(email: email, password: password);
+
+  static void login(String email, String password) async{
+    try{
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    }catch(e){
+      print(e);
+    }
   }
 }
